20fcb11c41e2a086939124f7fae93e30c4822e0f
Lena8829 <illenarionova@gmail.com>
2024-04-08 17:00:07 +0300

    simpleLightBox inst

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,1 +1,12 @@
-console.log("gallery");
+// console.log('gallery');
+
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+});
+
+console.log(SimpleLightbox);
