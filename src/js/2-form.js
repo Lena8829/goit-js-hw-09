2fb8939345f86b30b45cd1242ed9398c34714f47
Lena8829 <illenarionova@gmail.com>
2024-04-10 09:22:33 +0300

    task-2-form
    input event

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,32 @@
-console.log('form');
+/* 
+ * Використовуючи делегуваня, відстежуй на формі подію input 
+ 
+ * і щоразу записуй у локальне сховище об'єкт з полями email і message, у яких зберігай поточні значення полів форми. 
+ 
+ * Нехай ключем для сховища буде рядок "feedback-form-state".
+
+ Під час завантаження сторінки перевіряй стан сховища,
+ 
+ і якщо там є збережені дані, то заповнюй ними поля форми. 
+ 
+ В іншому випадку поля повинні бути порожніми.
+
+ Під час сабміту форми очищай сховище і поля форми,
+
+ а також виводь у консоль об'єкт з полями email, message та їхніми поточними значеннями.
+*/
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', textInput); // monitor the input event on the form
+
+function textInput(event) {
+  const savedData = {
+    email: form.email.value.trim(), // remove spaces with 'trim()'
+    message: form.message.value.trim(),
+  };
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(savedData)); // saving data and make obj ---> a string
+}
