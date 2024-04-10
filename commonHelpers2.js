cb7cfbd592670896e2c6332ec9c33448bd002857
Lena8829 <149965521+Lena8829@users.noreply.github.com>
2024-04-10 14:12:22 +0000

    Deploying to gh-pages from @ Lena8829/goit-js-hw-09@02cb800211c44f7cd204df5b4a630c4e8dfe4bc8 🚀

--- commonHelpers2.js
+++
@@ -1,2 +0,0 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */const a="feedback-form-state",e=document.querySelector(".feedback-form");e.addEventListener("input",m);function m(o){const t={email:e.email.value.trim(),message:e.message.value.trim()};localStorage.setItem(a,JSON.stringify(t))}
-//# sourceMappingURL=commonHelpers2.js.map
